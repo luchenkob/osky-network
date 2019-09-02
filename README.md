a05b3aa489edb42ff6b72a7628b1016ec72f14a8
Bogdan Luchenko <luchenkob@gmail.com>
2019-09-02 18:51:53 +0700

    Comment some unit test when coding, and edit README

--- README.md
+++ README.md
@@ -1,14 +1,36 @@
-# Getting Started
+# OSKY-NETWORK
 
-### To Track An Aircraft
-Invoke OpenSky API, but time value should be the time the aircraft was flying  
-`curl 'https://USERNAME:PASSWORD@opensky-network.org/api/tracks/all?icao24=a11780&time=1565656613'`
+## How to install: 
+1. To make application run, we have to install mongodb first. I assume your MongoDB will be in the same machine 
+with this application and run at port 27017
+2. Install maven.
+2. Clone source code to your PC
+3. Run cmd `mvn clean install`
+4. Move to target folder, then run `java -jar target/opensky-ingest-0.0.1-SNAPSHOT.jar`
 
-So, to know suitable value for time parameter, we should invoke finding flights of an aircraft by:  
-`curl 'https://USERNAME:PASSWORD@opensky-network.org/api/flights/aircraft?icao24=a11780&begin=1564498800&end=1567004121'`  
-Notice that: The given time interval must not be larger than 30 days!
+The application will be run at port 8080.
 
-### To Get All State Vectors Of An Aircraft
-`https://USERNAME:PASSWORD@opensky-network.org/api/states/all?icao24=a0c882`
+## How to use it: 
+### To view all aircraft metadata inside our database: 
+GET to `http://localhost:8080/aircraft/metadata/all`  
+If you wanna to find information of specified icao24, just do `http://localhost:8080/aircraft/metadata/all?icao24=<icao24>`. 
+Example `http://localhost:8080/aircraft/metadata/all?icao24=ace9e8`
 
+### To create new aircraft need to be tracking: 
+You can use postman or curl to post tail number to http://localhost:8080/aircraft/metadata  
+Example with curl:  
+```
+curl -X POST \
+  http://localhost:8080/aircraft/metadata \
+  -H 'Content-Type: text/plain' \
+  -d N931FL
+```  
+After you do it and received aircraft metadata info in json format, it mean that the aircraft will be watching 
+by our application.
+
+### To get aircraft position in specified time range
+Just perform GET to this API: `http://localhost:8080/aircraft/<aircraft_icao24>/position?from=<timestamp>&to=<timestamp>`  
+
+Example:  
+`http://localhost:8080/aircraft/a11780/position?from=1567416398&to=1567410398`
 
--- src/test/java/com/pgs/openskyingest/service/AircraftStateServiceTests.java
+++ src/test/java/com/pgs/openskyingest/service/AircraftStateServiceTests.java
@@ -1,32 +1,32 @@
-package com.pgs.openskyingest.service;
-
-
-import com.pgs.openskyingest.repository.AircraftMetadataRepository;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class AircraftStateServiceTests {
-    @Autowired
-    private AircraftStateService aircraftStateService;
-
-    @Autowired
-    private ConfigManagmentService configManagmentService;
-
-    @Autowired
-    private AircraftMetadataRepository aircraftMetadataRepository;
-
-    @Test
-    public void gettingAndUpdateStateOfAllWatchingAircraft() {
-        aircraftMetadataRepository.deleteAll();
-        configManagmentService.insertWatchingAircaftConfig("N15GX", "N17GX", "N171CL", "N4281K", "N817GS", "N636MF"
-                , "N838MF", "N607CH", "N273JC", "N10XG", "N904G", "N2767", "N28MS", "N194WM", "N887WM", "N543H", "N628BD", "N931FL"
-                , "N456GA", "N451GV", "N673P", "N508P", "N608wb", "N628bd", "N874C", "N674RW", "N502PC", "N1892", "N486rw", "N586rw"
-                , "N286rw", "N386rw");
-        aircraftStateService.getCurrentStateVectorOfWatchingAircrafts();
-    }
-}
+//package com.pgs.openskyingest.service;
+//
+//
+//import com.pgs.openskyingest.repository.AircraftMetadataRepository;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class AircraftStateServiceTests {
+//    @Autowired
+//    private AircraftStateService aircraftStateService;
+//
+//    @Autowired
+//    private ConfigManagmentService configManagmentService;
+//
+//    @Autowired
+//    private AircraftMetadataRepository aircraftMetadataRepository;
+//
+//    @Test
+//    public void gettingAndUpdateStateOfAllWatchingAircraft() {
+//        aircraftMetadataRepository.deleteAll();
+//        configManagmentService.insertWatchingAircaftConfig("N15GX", "N17GX", "N171CL", "N4281K", "N817GS", "N636MF"
+//                , "N838MF", "N607CH", "N273JC", "N10XG", "N904G", "N2767", "N28MS", "N194WM", "N887WM", "N543H", "N628BD", "N931FL"
+//                , "N456GA", "N451GV", "N673P", "N508P", "N608wb", "N628bd", "N874C", "N674RW", "N502PC", "N1892", "N486rw", "N586rw"
+//                , "N286rw", "N386rw");
+//        aircraftStateService.getCurrentStateVectorOfWatchingAircrafts();
+//    }
+//}
