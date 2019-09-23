35d3f1a33cc89381d1c2fe8ca08907da22890329
Bogdan Luchenko <luchenkob@gmail.com>
2019-09-23 22:16:57 +0700

    Add test

--- src/test/java/com/pgs/openskyingest/service/ConfigManagementServiceTests.java
+++ src/test/java/com/pgs/openskyingest/service/ConfigManagementServiceTests.java
@@ -21,18 +21,20 @@
 //    @Test
 //    public void addConfig() {
 //        aircraftMetadataRepository.deleteAll();
-//        configManagmentService.insertWatchingAircaftConfig("N15GX", "N17GX");
+//        configManagmentService.insertWatchingAircaftConfig("N15GX", "N17GX", "N171CL", "N4281K", "N817GS"
+//                , "N636MF", "N838MF", "N607CH", "N273JC", "N10XG", "N904GG", "N2767F", "N28MS", "N194WM", "N887WM", "N543HA"
+//                , "N628BD", "N931FL", "N456GA", "N451GV", "N673PH", "N2341B", "N23453", "N874CD", "N770LM", "N730LM");
 //
 //        for (AircraftMetadata meta : aircraftMetadataRepository.findAll()) {
 //            System.out.println(meta);
 //        }
-//        aircraftMetadataRepository.deleteAll();
+//        //aircraftMetadataRepository.deleteAll();
 //    }
-//
-//    @Test
-//    public void findAllIsTrackingAircraft() {
-//        configManagmentService.insertWatchingAircaftConfig("N171CL", "N4281K");
-//        aircraftMetadataRepository.findAircraftMetadataByIsTracking(Boolean.TRUE).forEach(meta -> System.out.println(meta.toString()));
-//    }
+////
+////    @Test
+////    public void findAllIsTrackingAircraft() {
+////        configManagmentService.insertWatchingAircaftConfig("N171CL", "N4281K");
+////        aircraftMetadataRepository.findAircraftMetadataByIsTracking(Boolean.TRUE).forEach(meta -> System.out.println(meta.toString()));
+////    }
 //}
 //
